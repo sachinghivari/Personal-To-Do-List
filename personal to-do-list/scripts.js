770714a52a9365f384120327a14d171608a345e8
Sachin Ghivari <117659692+sachinghivari@users.noreply.github.com>
2025-09-18 12:47:21 +0530

    Add files via upload

---
+++ personal to-do-list/scripts.js
@@ -0,0 +1,59 @@
+ 
+ let inputText = document.getElementById('input-text');
+ let addBtn = document.querySelector('.add-btn');
+ let newTask = document.querySelector('.new-task');
+
+
+
+   function getTask(){
+     let task = document.createElement('div');
+    task.classList.add('task');
+
+     let li = document.createElement('li');
+     li.innerText = `${inputText.value}`;
+     task.appendChild(li);
+
+     let checkBtn = document.createElement('button');
+     checkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+     checkBtn.classList.add('check');
+     task.appendChild(checkBtn);
+
+     let deleteBtn = document.createElement('button');
+     deleteBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+     deleteBtn.classList.add('delete');
+     task.appendChild(deleteBtn);
+
+    
+       checkBtn.addEventListener('click', function(){
+    li.style.textDecoration = "line-through";
+  });
+
+  deleteBtn.addEventListener('click', function(){
+    task.remove();
+  });
+      
+    if(inputText.value === ""){
+        alert("Please Enter Text!");
+    }else{
+       
+         newTask.appendChild(task);
+              inputText.value ="";
+    }
+   }
+
+
+  addBtn.addEventListener('click', function(){
+        getTask();
+  });
+
+    document.addEventListener('keydown', function(e){
+    if(e.key === "Enter"){
+         getTask();
+    }
+  });
+
+  
+
+
+
+   
